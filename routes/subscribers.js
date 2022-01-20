051913d070c28b5f1a19030ec1090217091d0491
kai <kai@kaiconsulting.co>
2022-01-20 14:08:56 -0700

    init repo

---
+++ routes/subscribers.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+// We want routes for: (subscribers)
+// Getting all 
+router.get('/', (req, res) => {
+
+})
+
+// Getting one
+router.get('/:id', (req, res) => {
+    
+})
+
+// Creating one
+router.post('/', (req, res) => {
+    
+})
+// Updating one
+router.patch('/:id', (req, res) => {
+    
+})
+
+// Deleting one 
+router.delete('/:id', (req, res) => {
+    
+})
+
+
+module.exports = router;
